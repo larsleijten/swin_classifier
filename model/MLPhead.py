d739d629dde30ac381f1ed3bc8c109c185b293b4
larsleijten <lars@13a.nl>
2023-03-12 23:34:51 +0100

    Fixed layernorm bug

--- model/MLPhead.py
+++ model/MLPhead.py
@@ -5,16 +5,18 @@ class MLPhead(nn.Module):
     def __init__(self, input_shape):
         super(MLPhead, self).__init__()
         self.fc1 = nn.Linear(input_shape, 512)
+        self.norm1 = nn.LayerNorm(512)
         self.dropout1 = nn.Dropout(0.5)
         self.fc2 = nn.Linear(512, 256)
+        self.norm2 = nn.LayerNorm(256)
         self.dropout2 = nn.Dropout(0.5)
         self.fc3 = nn.Linear(256, 1)
         
     def forward(self, x):
-        x = nn.LayerNorm(512)(self.fc1(x))
+        x = self.norm1(self.fc1(x))
         x = nn.functional.relu(x)
         x = self.dropout1(x)
-        x = nn.LayerNorm(256)(self.fc2(x))
+        x = self.norm2(self.fc2(x))
         x = nn.functional.relu(x)
         x = self.dropout2(x)
         x = torch.sigmoid(self.fc3(x))
